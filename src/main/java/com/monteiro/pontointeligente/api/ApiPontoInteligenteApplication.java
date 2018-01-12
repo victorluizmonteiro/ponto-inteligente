f6b04dbcec43b5b45ec8c20114638fd2a4c5f74f
Victor <victorluiz.monteiro@gmail.com>
2018-01-12 18:12:44 -0200

    Criando o projeto e adicionando depencencias do MySQL,H2,DevTools e JPA

---
+++ src/main/java/com/monteiro/pontointeligente/api/ApiPontoInteligenteApplication.java
@@ -0,0 +1,12 @@
+package com.monteiro.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPontoInteligenteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/monteiro/pontointeligente/api/ApiPontoInteligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.monteiro.pontointeligente.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiPontoInteligenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
